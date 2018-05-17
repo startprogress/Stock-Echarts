9fe3a0110f25f1c1629632962ca0039fcc2f59c3
startprogress <haobo_nice@163.com>
2018-05-17 23:49:57 +0800

    Show a month's data in a line chart

--- controllers.js
+++ controllers.js
@@ -2,21 +2,21 @@ var echarts = require('echarts');
 var request = require('request');
 
 exports.root = function(req, res) {
-  var url = "http://q.stock.sohu.com/hisHq?code=cn_600519&start=20180511&end=20180511";
+  var url = "http://q.stock.sohu.com/hisHq?code=cn_600519&start=20180411&end=20180511";
   request(url, function (err, response, body) {
     if (err) console.log("Fail to request " + url);
     if (!err && response.statusCode == 200) {
       var data = JSON.parse( body.substring(1, body.length - 2) );
-      var date = data['hq'][0][0];
       var code = data['code'];
-      var title = '日期: ' + date + '  股票代码: ' + code;
+      var title = '股票代码: ' + code;
       var stockData = new Array();
-
-      for(i in data['hq'][0].slice(1)){
-           stockData[i] = data['hq'][0].slice(1)[i];
+      var date = new Array();
+      var len = data['hq'].length;
+      for(i in data['hq']) {
+        stockData[len - 1 - i] = data['hq'][i][1];
+        date[len - 1 - i] = '"' + data['hq'][i][0].substring(5,10) + '"';
       }
-
-      res.render('index', {title: title, stockData: stockData});
+      res.render('index', {title: title, date: date, stockData: stockData});
     }
   });
 };
